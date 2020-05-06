f1dcae04f6e8711ff0c314514c042774caf7f430
RemyL <remy.lampure@free.fr>
2020-05-06 12:17:56 +0200

    POSP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_twolip.mk
+    $(LOCAL_DIR)/potato_twolip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_twolip-user \
-    lineage_twolip-userdebug \
-    lineage_twolip-eng
+    potato_twolip-user \
+    potato_twolip-userdebug \
+    potato_twolip-eng
